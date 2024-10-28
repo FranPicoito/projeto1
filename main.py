5fd1e647e77cbc406227b46d38797a3ee75b06da
Francisco Picoito <picoitof@gmail.com>
2024-10-28 15:01:05 +0000

    adicionei uma função para ler strings

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("yo")
+from utils import *
 
+if __name__ == '__main__':
+    nome = get_string("Aluno: ")
+    nota1 = get_float("Nota1: ")
+    nota2 = get_float("Nota2: ")
+
+    media = (nota1 + nota2) / 2
+
+    print(f"{nome} \t {media:.2f}")
---
+++ utils.py
@@ -0,0 +1,10 @@
+def get_int(msg):
+    i = input(msg)
+    return int(i)
+
+def get_float(msg):
+    f = input(msg)
+    return float(f)
+
+def get_string(msg):
+    return input(msg)
